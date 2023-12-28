b40a0a07bf91360820686619e292d7831ea6ca7c
blnche <peltierblanche@gmail.com>
2023-12-28 09:32:10 +0100

    testing

--- myApp.js
+++ myApp.js
@@ -1,4 +1,7 @@
-console.log("Hello World");
+const out = getStreamSomehow();
+const err = getStreamSomehow();
+const myConsole = new console.Console(out, err);
+myConsole.log("Hello World");
 let express = require('express');
 let app = express();
 
