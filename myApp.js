3f098758fdc3836a59c81a565508f9648e8e261f
blnche <peltierblanche@gmail.com>
2023-12-28 09:36:47 +0100

    .

--- myApp.js
+++ myApp.js
@@ -1,10 +1,7 @@
-const out = getStreamSomehow();
-const err = getStreamSomehow();
-const myConsole = new console.Console(out, err);
-myConsole.log("Hello World");
 let express = require('express');
 let app = express();
 
+console.log("Hello World");
 
 
 
