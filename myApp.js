9b6309f80cc2662b69e60a728a47f912fbec9a96
blnche <peltierblanche@gmail.com>
2024-01-12 08:23:09 +0100

    test done

--- myApp.js
+++ myApp.js
@@ -45,7 +45,6 @@ app.get('/:word/echo', function(req, res){
 app.route('/name').get(function(req, res){
     res.json({name: req.query.first + " " + req.query.last})
 }).post(function(req, res){
-    console.log(req.body.first);
     res.json({name: req.body.first + " " + req.body.last})
 })
 
