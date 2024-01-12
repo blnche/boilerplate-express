2c040b87d8a143ebe7fd35990b1d63f8cbd75931
blnche <peltierblanche@gmail.com>
2024-01-12 07:27:04 +0100

    logger

--- myApp.js
+++ myApp.js
@@ -8,22 +8,25 @@ console.log("Hello World");
 //     res.send('Hello Express');
 // });
 
-app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/views/index.html');
+// app.get('/', function(req, res) {
+//     res.sendFile(__dirname + '/views/index.html');
+// });
+
+
+// app.use('/public', express.static(__dirname + '/public'))
+
+// app.get('/json', function(req, res) {
+//     if(process.env.MESSAGE_STYLE === 'uppercase') {
+//         res.json( {'message': "Hello json".toUpperCase()});
+//     } else {
+//         res.json({'message': "Hello json"});
+//     };
+// })
+
+app.use('/', function(req, res, next) {
+    console.log(req.method +  " " + req.path + " - " + req.ip);
+    next();
 });
-
-
-app.use('/public', express.static(__dirname + '/public'))
-
-app.get('/json', function(req, res) {
-    if(process.env.MESSAGE_STYLE === 'uppercase') {
-        res.json( {'message': "Hello json".toUpperCase()});
-    } else {
-        res.json({'message': "Hello json"});
-    };
-})
-
-
 
 
 
