88ab49912088d9a58e7cad16153c9f4c74886a09
blnche <peltierblanche@gmail.com>
2024-01-12 07:35:55 +0100

    time server

--- myApp.js
+++ myApp.js
@@ -28,7 +28,12 @@ app.use('/', function(req, res, next) {
     next();
 });
 
-
+app.get('/now', function(req, res, next) {
+    req.time = new Date().toString();
+    next();
+}, function(req, res) {
+    res.send({time: req.time});
+});
 
 
 
